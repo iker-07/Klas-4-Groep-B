860dfbde3a3a141455e4b871904d67fbc0877487
iker-07 <24115010@student.hhs.nl>
2025-05-12 11:09:04 +0200

    kleine update v2

--- src/Main.java
+++ src/Main.java
@@ -15,109 +15,129 @@ abstract class Kamer {
         this.Thema = Thema;
         this.Opdracht = Opdracht;
         this.Vragen = Vragen;
-
     }
 
-    public String getNaam() {
-        return Naam;
+    // TEMPLATE METHOD
+    public final void kamerMenu() {
+        toonNaam();
+        toonBeschrijving();
+        voerOpdrachtUit();
+        stelVragen();
     }
 
-    public String getBeschrijving() {
-        return Beschrijving;
-    }
-    public String getThema() {
-        return Thema;
-    }
-    public String getOpdracht() {
-        return Opdracht;
-    }
-    public String getVragen() {
-        return Vragen;
-    }
-
-
-
-public void BinnenKomen() {
-    }
-
-
+    // ABSTRACTE STAPPEN
+    protected abstract void toonNaam();
+    protected abstract void toonBeschrijving();
+    protected abstract void voerOpdrachtUit();
+    protected abstract void stelVragen();
 }
+
+
+
+
+//public void BinnenKomen() {
+
+
+
+
 class kamer1 extends Kamer {
     public kamer1(String Naam, String Beschrijving,String Thema, String Opdracht, String Vragen) {
         super(Naam, Beschrijving, Thema, Opdracht, Vragen);
     }
+    @Override
+    protected void toonNaam() {
+        System.out.println("Kamernaam: " + Naam);
+    }
+    @Override
+    protected void toonBeschrijving() {
+        System.out.println("Beschrijving: " + Beschrijving);
+    }
+
+    @Override
+    protected void voerOpdrachtUit() {
+        System.out.println("Opdracht uitvoeren: " + Opdracht);
+    }
+
+    @Override
+    protected void stelVragen() {
+        System.out.println("Beantwoord de vraag: " + Vragen);
+    }
 }
 class kamer2 extends Kamer {
-    public kamer2(String Naam, String Beschrijving,String Thema, String Opdracht, String Vragen) {
+    public kamer2(String Naam, String Beschrijving, String Thema, String Opdracht, String Vragen) {
         super(Naam, Beschrijving, Thema, Opdracht, Vragen);
+    }
+
+    @Override
+    protected void toonNaam() {
+        System.out.println("Kamernaam: " + Naam);
+    }
+
+    @Override
+    protected void toonBeschrijving() {
+        System.out.println("Beschrijving: " + Beschrijving);
+    }
+
+    @Override
+    protected void voerOpdrachtUit() {
+        System.out.println("Opdracht uitvoeren: " + Opdracht);
+    }
+
+    @Override
+    protected void stelVragen() {
+        System.out.println("Beantwoord de vraag: " + Vragen);
     }
 }
-class kamer3 extends Kamer {
-    public kamer3(String Naam, String Beschrijving,String Thema, String Opdracht, String Vragen) {
-        super(Naam, Beschrijving, Thema, Opdracht, Vragen);
-    }
-}
-class kamer4 extends Kamer {
-    public kamer4(String Naam, String Beschrijving,String Thema, String Opdracht, String Vragen) {
-        super(Naam, Beschrijving, Thema, Opdracht, Vragen);
-    }
-}
-class kamer5 extends Kamer {
-    public kamer5(String Naam, String Beschrijving,String Thema, String Opdracht, String Vragen) {
-        super(Naam, Beschrijving, Thema, Opdracht, Vragen);
-    }
-}class kamer6 extends Kamer {
-    public kamer6(String Naam, String Beschrijving,String Thema, String Opdracht, String Vragen) {
-        super(Naam, Beschrijving, Thema, Opdracht, Vragen);
-    }
-}
+
 class Hub {
 }
 
-class Spel{
+class Spel {
     String Speler;
     String Hub;
     String Kamer;
 
 
-public static void StartGame() {
-    System.out.println("vul je GamerTag in:");
-    Scanner GamerTag = new Scanner(System.in);
-    String Naam = GamerTag.nextLine();
-    System.out.println("Naam: " + Naam );
-
-
-
-}
-
-public static void KiesKamer() {
-    System.out.println("Kies een kamer");
-    Scanner Kamer = new Scanner(System.in);
-    int Kamernr = Kamer.nextInt();
-
-    switch (Kamernr) {
-        case 1:
-            break;
-        case 2:
-            System.out.println("Kies een kamer");
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-        case 6:
-            break;
-    }
-
-
-
+    public static void StartGame() {
+        System.out.println("vul je GamerTag in:");
+        Scanner GamerTag = new Scanner(System.in);
+        String Naam = GamerTag.nextLine();
+        System.out.println("Naam: " + Naam);
 
 
     }
 
+    public static void KiesKamer() {
+        System.out.println("Kies een kamer (1-6):");
+        Scanner scanner = new Scanner(System.in);
+        int keuze = scanner.nextInt();
+
+        Kamer kamer = null;
+        switch (keuze) {
+            case 1:
+                kamer = new kamer1("Kamer 1", "Een mysterieuze kamer", "Mysterie", "Zoek de sleutel", "Wat is 2 + 2?");
+                break;
+            case 2:
+                kamer = new kamer2("Kamer 2", "Een enge kamer", "Horror", "Ontsnap uit de kooi", "Wat is je grootste angst?");
+                break;
+            // Voeg de rest toe
+            default:
+                System.out.println("Ongeldige keuze.");
+                return;
+        }
+
+        kamer.kamerMenu();  // Template method uitvoeren
+    }
 }
+
+
+
+
+
+
+
+
+
 class Speler{
     String Positite;
     String StatusPet;

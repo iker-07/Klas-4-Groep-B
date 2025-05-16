2b06b5f9601928246cfe3ceee912076c4aa64424
Aditya200413 <24060682@student.hhs.nl>
2025-05-16 12:52:03 +0200

    namen klasses veranderd naar observer pattern

---
+++ src/MonsterObserver.java
@@ -0,0 +1,11 @@
+public class MonsterObserver implements Observer {
+    @Override
+    public void correctAntwoord() {
+        System.out.println("Het monster is verslagen!");
+    }
+
+    @Override
+    public void foutAntwoord() {
+        System.out.println("Het monster valt aan!");
+    }
+}
--- src/Sprintplanning.java
+++ src/Sprintplanning.java
@@ -1,16 +1,25 @@
 import java.util.Map;
+
 public class Sprintplanning extends Kamer {
+    private PuzzelEventManager eventManager;
+
     public Sprintplanning() {
         super(
                 "Sprint Planning kamer",
                 "Sprint Planning",
                 "Scrum Event",
-                "Koppel termen aan juiste uitleg",
+                "Koppel termen aan juiste uitleg\n",
                 new PuzzelVraagStrategie(Map.of(
                         "Het moment waarop werk voor de sprint wordt geselecteerd", "Sprint Planning",
                         "Het doel voor de aankomende sprint", "Sprint Doel"
                 ))
         );
+
+        // Maak event manager aan en voeg observers toe
+        eventManager = new PuzzelEventManager();
+        eventManager.addObserver(new Deur());
+        eventManager.addObserver(new MonsterObserver());
+        eventManager.addObserver(new StatusDisplay());
     }
 
     @Override
@@ -20,7 +29,12 @@ public class Sprintplanning extends Kamer {
     protected void toonBeschrijving() { System.out.println("Beschrijving: " + Beschrijving); }
 
     @Override
-    protected void voerOpdrachtUit() { System.out.println("Opdracht uitvoeren: " + Opdracht); }
+    protected void voerOpdrachtUit() {
+        System.out.println("Opdracht uitvoeren: " + Opdracht);
+        // Bijvoorbeeld: zodra een antwoord juist/fout is, roep eventManager aan
+        // eventManager.notifyCorrectAntwoord();
+        // of eventManager.notifyFoutAntwoord();
+    }
 
     @Override
     protected int getKamerNummer() { return 2; }

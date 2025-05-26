fea1810a0b3ad7a5afb308b0099f0035082f6e91
aideenikhab23 <aideenikhab@gmail.com>
2025-05-26 12:27:54 +0200

    strategieen aangepast

--- src/Speler.java
+++ src/Speler.java
@@ -11,11 +11,6 @@ class Speler {
         this.positie = "Hub";
         this.statusPet = "Gezond";
         this.gamertag = gamertag;
-        this.voortgang = DatabaseManager.laadVoortgang(gamertag);
-        System.out.println("Voortgang geladen voor " + gamertag + ": " + voortgang);
-    }
-
-    public Speler(String spelerNaam) {
     }
 
     public void Move(String nieuwePositie) {
@@ -35,20 +30,5 @@ class Speler {
         System.out.println("======================");
     }
 
-    public void verhoogVoortgang() {
-        voortgang++;
-        System.out.println("Nieuwe voortgang: " + voortgang);
-    }
 
-    public void slaVoortgangOp() {
-        DatabaseManager.slaVoortgangOp(gamertag, voortgang);
-    }
-
-    public int getVoortgang() {
-        return voortgang;
-    }
-
-    public String getGamertag() {
-        return gamertag;
-    }
 }

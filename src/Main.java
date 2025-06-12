6dce632ff20de239eb7067fcd394b7d77fd0c1f1
iker-07 <24115010@student.hhs.nl>
2025-06-12 10:41:59 +0200

    Merge branch 'aditya'
    # Conflicts:
    #	src/Entiteiten/SpelController.java
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,22 @@
 import Entiteiten.SpelController;
 import java.util.Scanner;
+import Entiteiten.Naam;
 
-import static Entiteiten.Speler.spelerNaam;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Goedendag! Welkom bij het Scrum Escape Game!");
         System.out.println("Veel succes en plezier!\n");
 
+        Naam naam = new Naam();
+        naam.naamding();
+        boolean Naamvery = false;
+
         Scanner scanner = new Scanner(System.in);
         SpelController spelController = new SpelController(scanner);
-        System.out.println("Wat is uw naam?");
+        if (Naamvery = true)
 
-        spelerNaam = scanner.nextLine() ;
-        while(spelerNaam.trim().isEmpty()) {
-            System.out.println("Naam mag niet leeg zijn. Probeer opnieuw.");
-            spelerNaam = scanner.nextLine();
-        }
-        if(!spelerNaam.trim().isEmpty()){
-            System.out.println("Hallo " + spelerNaam + ". Veel succes!");
             spelController.start();
-        }
 
-        scanner.close();
     }
 }

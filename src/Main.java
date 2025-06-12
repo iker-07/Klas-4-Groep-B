36ad4b3cd9fb819305bb5e5b3cf2ac53005532d2
Timwerker <24001961@student.hhs.nl>
2025-06-12 10:24:55 +0200

    Naam

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import Entiteiten.Speler;
 import Entiteiten.SpelController;
 import java.util.Scanner;
 
@@ -9,12 +8,19 @@ public class Main {
         System.out.println("Goedendag! Welkom bij het Scrum Escape Game!");
         System.out.println("Veel succes en plezier!\n");
 
-
         Scanner scanner = new Scanner(System.in);
-        SpelController spelController = new SpelController(scanner); System.out.println("wat is uw naam?");
+        SpelController spelController = new SpelController(scanner);
+        System.out.println("Wat is uw naam?");
+
         spelerNaam = scanner.nextLine() ;
-        System.out.println("Hallo "+ spelerNaam + ". Veel succes!");
-        spelController.start();
+        while(spelerNaam.trim().isEmpty()) {
+            System.out.println("Naam mag niet leeg zijn. Probeer opnieuw.");
+            spelerNaam = scanner.nextLine();
+        }
+        if(!spelerNaam.trim().isEmpty()){
+            System.out.println("Hallo " + spelerNaam + ". Veel succes!");
+            spelController.start();
+        }
 
         scanner.close();
     }
